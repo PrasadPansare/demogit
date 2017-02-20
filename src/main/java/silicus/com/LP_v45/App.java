7472de5d1ba2f36cac0d3f787d46a4ef4a943f81
PPansare <PPansare@SILICUS518.silicus.com>
2017-02-20 16:40:23 +0530

    created project structure.

---
+++ src/main/java/silicus/com/LP_v45/App.java
@@ -0,0 +1,13 @@
+package silicus.com.LP_v45;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
